296d9b6b890240aa8d25a72c718eaf4c8bdf13e0
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-16 12:04:48 +0100

    test_V07

--- toDo/app.js
+++ toDo/app.js
@@ -1,10 +1,13 @@
-const { argv } = require('./config/yargs').argv;
+const argv = require('./config/yargs').argv;
+
+const porHacer = require('./porHacer/porHacer');
 
 let comando = argv._[0];
 
 switch (comando) {
     case 'crear':
-        console.log('Crear tarea');
+        let tarea = porHacer.crear(argv.descripcion);
+        console.log(tarea);
         break;
     case 'listar':
         console.log('Mostrar todas las tareas');
