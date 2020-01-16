c9d46f94e2c3d1a2c733ce4f6c22dcfd507f8dc0
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-16 17:49:02 +0100

    test_V010

---
+++ test/motor.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById('btn').addEventListener('click', function() {
+    // btn.style.backgroundColor = 'green';
+    location.href = 'newPage.html';
+});
+
+
+
+// function funcionJS() {
+//     let btn = document.getElementById('btn');
+//     btn.style.backgroundColor = 'green';
+//     location.href = 'newPage.html';
+// }
