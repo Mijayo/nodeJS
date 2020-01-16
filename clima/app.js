3b680b989bf45d5c553bf2d7021644bb753653d2
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-16 21:01:27 +0100

    test_V013

--- clima/app.js
+++ clima/app.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+
+
 const argv = require('yargs').options({
     direccion: {
         alias: 'd',
@@ -6,4 +9,23 @@ const argv = require('yargs').options({
     }
 }).argv;
 
-console.log(argv.direccion);
+// console.log(argv.direccion);
+
+const codedURL = decodeURI(argv.direccion);
+console.log(codedURL);
+
+const instance = axios.create({
+    baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${codedURL}`,
+    headers: {
+        'x-rapidapi-key': '53cebbfac0mshc8293123cb0fdeap1c27f5jsn416fd006978c',
+        'x-rapidapi-host': 'devru-latitude-longitude-find-v1.p.rapidapi.com'
+    }
+});
+
+instance.get()
+    .then(resp => {
+        console.log(resp.data.Results[0]);
+    })
+    .catch(err => {
+        console.log(err);
+    });
