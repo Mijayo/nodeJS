9154a215ea10acec3b5573b6a60261807fc0f1d2
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-18 11:40:45 +0100

    test_V020

--- webServer/server.js
+++ webServer/server.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
 
-
 app.use(express.static(__dirname + '/public'));
 
-// app.get('/', function(req, res) {
-//     // res.send('<h1 style="font-size: 150px">Hola Mundo</h1>');
+// Express HBS engine
+app.set('view engine', 'hbs');
 
-//     let salida = {
-//         nombre: 'Google',
-//         url: 'https://www.google.com'
-//     };
-//     res.send(salida);
-// });
+app.get('/', function(req, res) {
+    // res.send('<h1 style="font-size: 150px">Hola Mundo</h1>');
+
+    // let salida = {
+    //     nombre: 'Google',
+    //     url: 'https://www.google.com'
+    // };
+    // res.send(salida);
+
+    res.render('home', {
+        nombre: 'Diego',
+        txt: 'Bacon ipsum dolor amet pancetta porchetta chislic spare ribs ground round Chislic tongue alcatra cow swine ball tip tri-tip pork chop capicola meatball. Shank fatback shankle, strip steak chislic kielbasa tongue chicken ground round. Jowl shankle t-bone, sirloin jerky cupim turducken bacon chuck ground round brisket drumstick hamburger biltong. Beef ribs corned beef biltong shank pork belly.',
+        anio: new Date().getFullYear(),
+        btn: 'más info'
+    });
+
+});
 
 app.listen(3000, () => {
     console.log('Escuando en el puerto 3000');
