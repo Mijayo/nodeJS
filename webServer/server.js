d022aef9e04698228b176b3b8b0c52535e70c722
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-19 10:12:24 +0100

    test_V021

--- webServer/server.js
+++ webServer/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
+const hbs = require('hbs');
+
+
+hbs.registerPartials(__dirname + '/views/parciales');
+
 
 app.use(express.static(__dirname + '/public'));
-
 // Express HBS engine
 app.set('view engine', 'hbs');
 
@@ -16,6 +20,17 @@ app.get('/', function(req, res) {
     // res.send(salida);
 
     res.render('home', {
+        nombre: 'Diego',
+        txt: 'Bacon ipsum dolor amet pancetta porchetta chislic spare ribs ground round Chislic tongue alcatra cow swine ball tip tri-tip pork chop capicola meatball. Shank fatback shankle, strip steak chislic kielbasa tongue chicken ground round. Jowl shankle t-bone, sirloin jerky cupim turducken bacon chuck ground round brisket drumstick hamburger biltong. Beef ribs corned beef biltong shank pork belly.',
+        anio: new Date().getFullYear(),
+        btn: 'más info'
+    });
+
+});
+
+app.get('/about', function(req, res) {
+
+    res.render('about', {
         nombre: 'Diego',
         txt: 'Bacon ipsum dolor amet pancetta porchetta chislic spare ribs ground round Chislic tongue alcatra cow swine ball tip tri-tip pork chop capicola meatball. Shank fatback shankle, strip steak chislic kielbasa tongue chicken ground round. Jowl shankle t-bone, sirloin jerky cupim turducken bacon chuck ground round brisket drumstick hamburger biltong. Beef ribs corned beef biltong shank pork belly.',
         anio: new Date().getFullYear(),
