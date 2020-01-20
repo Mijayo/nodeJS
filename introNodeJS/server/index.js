ac61435875effc16fb5178a6dacb4703d9712064
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-20 18:53:30 +0100

    test_V023

--- introNodeJS/server/index.js
+++ introNodeJS/server/index.js
@@ -12,6 +12,9 @@ app.set('view engine', 'pug');
 // Add vistas
 app.set('views', path.join(__dirname, './views'));
 
+// Cargar una carpeta estatica llamada public
+app.use(express.static('public'));
+
 // cargar las rutas
 app.use('/', routes());
 
