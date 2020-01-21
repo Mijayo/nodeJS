b2da0cb50ec9b290dd692313fc5c2a42ecfb9dc6
Diego <48282657+Mijayo@users.noreply.github.com>
2020-01-21 20:00:53 +0100

    test_V026

--- restServer/server/server.js
+++ restServer/server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -20,9 +22,18 @@ app.post('/usuario', function(req, res) {
 
     let body = req.body;
 
-    res.json({
-        body
-    });
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            nombre: 'El nombre es necesario'
+        });
+    } else {
+
+        res.json({
+            body
+        });
+    }
+
 });
 
 app.put('/usuario/:id', function(req, res) {
@@ -39,6 +50,6 @@ app.delete('/usuario', function(req, res) {
 });
 
 
-app.listen(3000, () => {
-    console.log('Escuchando puerto', 3000);
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto', process.env.PORT);
 });
